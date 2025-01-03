da49f6ea1ff2ff7a33ace2616c1bd25d8041cb93
DanaiWee <Danai_wee@hotmail.com>
2025-01-03 15:14:41 +0700

    added notifications controller to backend

---
+++ backend/routes/notification.route.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+import { protectRoute } from '../middleware/protectRoute.js';
+import { deleteNotifications, getNotification } from '../controllers/notification.controller.js';
+
+const router = express.Router();
+
+router.get("/", protectRoute, getNotification);
+router.delete("/", protectRoute, deleteNotifications);
+
+export default router;
